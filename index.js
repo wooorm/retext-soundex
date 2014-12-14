9b6f2fca7e3d67b3b1f17c6b6d5cc6dcfed4f74a
wooorm <tituswormer@gmail.com>
2014-12-14 12:58:18 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * Dependencies.
  */
 
-var Retext = require('wooorm/retext@0.4.0');
-var soundex = require('wooorm/retext-soundex@0.1.4');
-var stemmer = require('wooorm/retext-porter-stemmer@0.2.2');
-var dom = require('wooorm/retext-dom@0.2.4');
-var visit = require('wooorm/retext-visit@0.2.2');
+var Retext = require('wooorm/retext@0.5.0-rc.1');
+var soundex = require('wooorm/retext-soundex@0.1.5');
+var stemmer = require('wooorm/retext-porter-stemmer@0.2.4');
+var dom = require('wooorm/retext-dom@0.3.0');
+var visit = require('wooorm/retext-visit@0.2.5');
 
 /**
  * Retext.
